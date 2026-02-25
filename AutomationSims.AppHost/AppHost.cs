a0e535d470f1a8b212e55fefcb78e97e672eb5d0
GamiDroid <kevin.d.damen@gmail.com>
2026-02-25 20:54:54 +0100

    Add SvgViewer web project

--- AutomationSims.AppHost/AppHost.cs
+++ AutomationSims.AppHost/AppHost.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.OpcUaServer_Web>("opcuaserver-web");
 
 builder.AddProject<Projects.CoffeeMaker_Api>("coffeemaker-api");
 
+builder.AddProject<Projects.SvgViewer_Web>("svgviewer-web");
+
 builder.Build().Run();
---
+++ SvgViewer.Web/wwwroot/scripts/index.js
@@ -0,0 +1,8 @@
+﻿import { SVG } from '@svgdotjs/svg.js'
+
+function loadSvg() {
+
+    var draw = SVG().addTo('#svg-container').size(300, 300)
+    var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+
+}
